fdf4f7596ca7cab44b9d6b524ff4f6c0bcd6f6cf
Severin Ibarluzea <seveibar@gmail.com>
2018-12-11 18:02:20 -0500

    survey sample cases

---
+++ src/components/Survey/all-question-types.sample.js
@@ -0,0 +1,65 @@
+// @flow
+
+import type { SurveyMaterialFormat } from "../../material-survey-format.js.flow"
+
+export default ({
+  questions: [
+    {
+      type: "dropdown",
+      title: "How do you feel about this dropdown?",
+      name: "dropdown-feeling",
+      choices: ["It's great!", "Not a fan", "I could get used to it"]
+    },
+    {
+      type: "boolean",
+      title: "Do you like boolean questions?",
+      name: "like-boolean"
+    },
+    {
+      type: "radiogroup",
+      title: "What kind of bear is strongest?",
+      name: "strongest-bear",
+      choices: ["Brown Bear", "Grizzly Bear", "Black Bear", "Polar Bear"]
+    },
+    {
+      type: "checkbox",
+      title: "What kind of bears are brown?",
+      name: "brown-bear",
+      choices: ["Brown Bear", "Grizzly Bear", "Black Bear", "Polar Bear"]
+    },
+    {
+      type: "choiceranker",
+      title: "What kind of sauce do you like?",
+      name: "sauce-ranking",
+      choicesAtOnce: 3,
+      trials: 3,
+      choices: ["Marinara", "Ranch", "Ketchup", "Alfredo"]
+    },
+    {
+      type: "slider",
+      title: "How large is a mouse?",
+      name: "mouse-size",
+      max: 10,
+      min: 0,
+      unit: "inches"
+    },
+    {
+      type: "rating",
+      title: "How important is global warming?",
+      name: "global-warming",
+      maxRateDescription: "Extremely Important",
+      minRateDescription: "Not Important"
+    },
+    {
+      type: "us-region",
+      title: "What region of the U.S. would you like to live in?",
+      name: "preferred-us-region"
+    },
+    {
+      type: "api-autocomplete",
+      title: "What's your zip code?",
+      name: "zip-code",
+      requestUrl: "/zip-code"
+    }
+  ]
+}: SurveyMaterialFormat)
--- src/components/Survey/index.story.js
+++ src/components/Survey/index.story.js
@@ -4,77 +4,24 @@ import React from "react"
 
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
-import type { SurveyMaterialFormat } from "../../material-survey-format.js.flow"
 import { zipCodeAutocompleteRequest } from "../APIAutocompleteQuestion/index.story.js"
+import allQuestionsSurvey from "./all-question-types.sample.js"
+import workaroundInterview from "./workaround-interview.sample.js"
 
 import Survey from "./"
 
-storiesOf("Survey", module).add("Basic", () => (
-  <Survey
-    form={
-      ({
-        questions: [
-          {
-            type: "dropdown",
-            title: "How do you feel about this dropdown?",
-            name: "dropdown-feeling",
-            choices: ["It's great!", "Not a fan", "I could get used to it"]
-          },
-          {
-            type: "boolean",
-            title: "Do you like boolean questions?",
-            name: "like-boolean"
-          },
-          {
-            type: "radiogroup",
-            title: "What kind of bear is strongest?",
-            name: "strongest-bear",
-            choices: ["Brown Bear", "Grizzly Bear", "Black Bear", "Polar Bear"]
-          },
-          {
-            type: "checkbox",
-            title: "What kind of bears are brown?",
-            name: "brown-bear",
-            choices: ["Brown Bear", "Grizzly Bear", "Black Bear", "Polar Bear"]
-          },
-          {
-            type: "choiceranker",
-            title: "What kind of sauce do you like?",
-            name: "sauce-ranking",
-            choicesAtOnce: 3,
-            trials: 3,
-            choices: ["Marinara", "Ranch", "Ketchup", "Alfredo"]
-          },
-          {
-            type: "slider",
-            title: "How large is a mouse?",
-            name: "mouse-size",
-            max: 10,
-            min: 0,
-            unit: "inches"
-          },
-          {
-            type: "rating",
-            title: "How important is global warming?",
-            name: "global-warming",
-            maxRateDescription: "Extremely Important",
-            minRateDescription: "Not Important"
-          },
-          {
-            type: "us-region",
-            title: "What region of the U.S. would you like to live in?",
-            name: "preferred-us-region"
-          },
-          {
-            type: "api-autocomplete",
-            title: "What's your zip code?",
-            name: "zip-code",
-            requestUrl: "/zip-code"
-          }
-        ]
-      }: SurveyMaterialFormat)
-    }
-    autocompleteRequest={zipCodeAutocompleteRequest}
-    onFinish={action("onFinish")}
-  />
-))
+storiesOf("Survey", module)
+  .add("All Question Types", () => (
+    <Survey
+      form={allQuestionsSurvey}
+      autocompleteRequest={zipCodeAutocompleteRequest}
+      onFinish={action("onFinish")}
+    />
+  ))
+  .add("WorkAround Interview", () => (
+    <Survey
+      form={workaroundInterview}
+      autocompleteRequest={zipCodeAutocompleteRequest}
+      onFinish={action("onFinish")}
+    />
+  ))
