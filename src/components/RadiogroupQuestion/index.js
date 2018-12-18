e4fc5370b598abe8090e551f6819736cb8b56da1
Severin Ibarluzea <seveibar@gmail.com>
2018-12-18 11:41:35 -0500

    radio group question hasOther implementation

--- src/components/RadiogroupQuestion/index.js
+++ src/components/RadiogroupQuestion/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import type { RadioGroupQuestion } from "../../material-survey-format.js.flow"
+import type { RadioGroupQuestion as RadioGroupQuestionType } from "../../material-survey-format.js.flow"
 import React, { useState } from "react"
 import useQuestionAnswer from "../../hooks/use-question-answer"
 import Radio from "@material-ui/core/Radio"
@@ -8,6 +8,7 @@ import QuestionContainer from "../QuestionContainer"
 import Button from "@material-ui/core/Button"
 import styled from "styled-components"
 import QuestionText from "../QuestionText"
+import TextField from "@material-ui/core/TextField"
 
 const RadioItem = styled(Button)`
   && {
@@ -19,17 +20,36 @@ const RadioItem = styled(Button)`
   }
 `
 
-export default ({
+const RadioItemNoRipple = styled.div`
+  && {
+    display: flex;
+    margin-top: 1px;
+    padding: 0;
+    padding-right: 20px;
+    align-items: center;
+    input {
+      font-size: 14px;
+      font-weight: 500;
+    }
+  }
+`
+
+export default function RadioGroupQuestion({
   question,
   onChangeAnswer
 }: {
-  question: RadioGroupQuestion,
+  question: RadioGroupQuestionType,
   onChangeAnswer: Function
-}) => {
+}) {
   const [{ answer, error }, changeAnswer] = useQuestionAnswer(
     question,
     onChangeAnswer,
     question.defaultAnswer || undefined
+  )
+  const [otherText, changeOtherText] = useState(
+    question.choices.some(c => c === answer || (c || {}).value === answer)
+      ? ""
+      : answer
   )
   const choices = question.choices.map(c =>
     typeof c === "string" ? { text: c, value: c } : c
@@ -47,6 +67,26 @@ export default ({
           <QuestionText>{text}</QuestionText>
         </RadioItem>
       ))}
+      {question.hasOther && (
+        <RadioItemNoRipple tabIndex={-1}>
+          <Radio
+            checked={answer === otherText}
+            onClick={() => changeAnswer(otherText)}
+          />
+          <TextField
+            // inputProps={{ style: { fontSize: 8 } }}
+            value={otherText}
+            onChange={e => {
+              if (answer === otherText) {
+                changeOtherText(e.target.value)
+                changeAnswer(e.target.value)
+              } else {
+                changeOtherText(e.target.value)
+              }
+            }}
+          />
+        </RadioItemNoRipple>
+      )}
     </QuestionContainer>
   )
 }
--- src/components/RadiogroupQuestion/index.story.js
+++ src/components/RadiogroupQuestion/index.story.js
@@ -7,13 +7,27 @@ import { action } from "@storybook/addon-actions"
 
 import RadiogroupQuestion from "./"
 
-storiesOf("RadiogroupQuestion", module).add("Basic", () => (
-  <RadiogroupQuestion
-    question={{
-      name: "radiogroup-example",
-      title: "What's the best syrup?",
-      type: "radiogroup",
-      choices: ["Maple Syrup", "Corn Syrup", "Cough Syrup"]
-    }}
-  />
-))
+storiesOf("RadiogroupQuestion", module)
+  .add("Basic", () => (
+    <RadiogroupQuestion
+      onChangeAnswer={action("onChangeAnswer")}
+      question={{
+        name: "radiogroup-example",
+        title: "What's the best syrup?",
+        type: "radiogroup",
+        choices: ["Maple Syrup", "Corn Syrup", "Cough Syrup"]
+      }}
+    />
+  ))
+  .add("hasOther", () => (
+    <RadiogroupQuestion
+      onChangeAnswer={action("onChangeAnswer")}
+      question={{
+        name: "radiogroup-example",
+        title: "What's the best syrup?",
+        type: "radiogroup",
+        hasOther: true,
+        choices: ["Maple Syrup", "Corn Syrup", "Cough Syrup"]
+      }}
+    />
+  ))
